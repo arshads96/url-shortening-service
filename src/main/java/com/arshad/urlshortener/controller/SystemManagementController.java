9e10cba8bc14c624c7805de991d47af30fc555fa
Arshad <arshads96@gmail.com>
2024-11-17 22:20:49 +0530

    Exposing metric api "top-shortened-urls" on management port 8090

---
+++ src/main/java/com/arshad/urlshortener/controller/SystemManagementController.java
@@ -0,0 +1,25 @@
+package com.arshad.urlshortener.controller;
+
+import com.arshad.urlshortener.contract.UrlService;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+import java.util.stream.Collectors;
+
+@Component
+@Endpoint(id = "top-shortened-urls")
+public class SystemManagementController {
+
+    private final UrlService service;
+
+    public SystemManagementController(UrlService service) {
+        this.service = service;
+    }
+
+   @ReadOperation
+    public String getTopShortenedUrls() {
+        return service.getDomainMetrics().stream()
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+}
